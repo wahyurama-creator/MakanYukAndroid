09c05a66b9c20f0859af02e5a4cf80a85cb04038
wahyurama-creator <wahyuramas32@gmail.com>
2021-02-25 12:33:42 +0700

    Create Sign In and Sign Up UI

---
+++ app/src/main/java/com/dev/makanyuk/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package com.dev.makanyuk.ui.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.dev.makanyuk.MainActivity
+import com.dev.makanyuk.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 4000)
+    }
+}
