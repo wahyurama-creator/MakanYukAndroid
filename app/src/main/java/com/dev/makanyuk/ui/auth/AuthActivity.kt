20f8a1ba7649d879646378d4206a39a9979860df
wahyurama-creator <wahyuramas32@gmail.com>
2021-02-26 11:06:03 +0700

    Create Sign In Success Page & Auth Navigation in Sign Up Fragment

--- app/src/main/java/com/dev/makanyuk/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/dev/makanyuk/ui/auth/AuthActivity.kt
@@ -2,11 +2,25 @@ package com.dev.makanyuk.ui.auth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
 import com.dev.makanyuk.R
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        val pageRequest = intent.getIntExtra("page_request", 0)
+        if (pageRequest == 2) {
+            val navOption = NavOptions.Builder()
+                .setPopUpTo(R.id.signInFragment, true)
+                .build()
+
+            Navigation.findNavController(
+                findViewById(R.id.fragmentAuth)
+            )
+                .navigate(R.id.action_signInFragment_to_signUpFragment, null, navOption)
+        }
     }
 }
--- app/src/main/java/com/dev/makanyuk/ui/auth/signin/SignInFragment.kt
+++ app/src/main/java/com/dev/makanyuk/ui/auth/signin/SignInFragment.kt
@@ -1,11 +1,14 @@
 package com.dev.makanyuk.ui.auth.signin
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.dev.makanyuk.R
+import com.dev.makanyuk.ui.auth.AuthActivity
+import kotlinx.android.synthetic.main.fragment_sign_in.*
 
 class SignInFragment : Fragment() {
 
@@ -15,5 +18,15 @@ class SignInFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btn_register.setOnClickListener {
+            val signUp = Intent(activity, AuthActivity::class.java)
+            signUp.putExtra("page_request", 2)
+            startActivity(signUp)
+        }
     }
 }
